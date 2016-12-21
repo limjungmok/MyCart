0707d2a434d3a66f0b321ad02773d29bec3de3f9
JM <limjungmok@gmail.com>
2016-12-21 10:42:54 +0900

    first

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+
+module.exports = {
+
+    /*
+    webpack-dev-server를 콘솔이 아닌 자바스크립트로 실행 할 땐,
+    HotReloadingModule 를 사용하기 위해선 dev-server 클라이언트와
+    핫 모듈을 따로 entry 에 넣어주어야 합니다.
+    */
+
+    entry: './src/index.js',
+
+    output: {
+        path: __dirname + '/public',
+        filename: 'bundle.js'
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ['react-hot', 'babel?' + JSON.stringify({
+                    cacheDirectory: true,
+                    presets: ['es2015', 'react']
+                })],
+                exclude: /node_modules/,
+            }
+        ]
+    }
+};
